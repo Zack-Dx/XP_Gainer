95b85ed4b59d6f759d02b2cfbb24f5b96ad56a0f
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2024-03-22 12:55:00 +0530

    Celebration Confetti Added.

--- src/app/playlists/[...slug]/page.tsx
+++ src/app/playlists/[...slug]/page.tsx
@@ -5,6 +5,7 @@ import { FaPlay, FaPause } from "react-icons/fa";
 import { Video } from "@/interfaces";
 import { VideoPlayer } from "@/components/ui/video-player";
 import { ProgressBar } from "@/components/ui/progress-bar";
+import Confetti from "react-confetti";
 
 interface PageProps {
   params: {
@@ -18,13 +19,30 @@ const Page = ({ params }: PageProps) => {
   const [activeVideoStatus, setActiveVideoStatus] = useState<boolean>(false);
   const [completedVideos, setCompletedVideos] = useState<string[]>([]);
   const [resetVideo, setResetVideo] = useState<boolean>(false);
+  const [celebrate, setCelebrate] = useState(false);
+  const progress = (completedVideos.length / videos.length) * 100 || 0;
 
   let [course, instructor] = params.slug;
   course = decodeURIComponent(course);
   instructor = decodeURIComponent(instructor);
 
   useEffect(() => {
-    // Reset resetVideo state after the video has been reset
+    if (progress !== 100) {
+      return;
+    }
+
+    // Celebration timeout for 5 seconds
+    setCelebrate(true);
+    const timeoutId = setTimeout(() => {
+      setCelebrate(false);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [progress]);
+
+  useEffect(() => {
     if (resetVideo) {
       setResetVideo(false);
     }
@@ -55,10 +73,9 @@ const Page = ({ params }: PageProps) => {
     }
   };
 
-  const progress = (completedVideos.length / videos.length) * 100 || 0;
-
   return (
     <div>
+      {celebrate && <Confetti />}
       <div className="p-4 flex flex-col md:flex-row md:justify-between items-center rounded-md bg-gradient-to-r from-pink-500 to-purple-500">
         <h1 className="text-base font-semibold text-white mb-4 md:mb-0 md:mr-6">
           Instructor: {instructor} ({course.toUpperCase()})
@@ -90,10 +107,12 @@ const Page = ({ params }: PageProps) => {
                   className="p-5 border-b-[0.5px] border-gray-700 transition-all cursor-pointer flex items-center bg-black hover:bg-slate-900 font-semibold text-base rounded-md"
                 >
                   <div className="mr-2">
-                    <input
-                      onChange={() => handlePlaylistProgress(name)}
-                      type="checkbox"
-                    />
+                    {progress < 100 ? (
+                      <input
+                        onChange={() => handlePlaylistProgress(name)}
+                        type="checkbox"
+                      />
+                    ) : null}
                   </div>
 
                   <div
